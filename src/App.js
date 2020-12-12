b8954dbf335719917061f04c5d6b22cd925a2681
atzt <zak.kubicek@gmail.com>
2020-12-12 16:07:16 -0500

    Create CI/CD Workflow
    - Fix CI/CD Pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. I shouldn't see this text.
+          Edit <code>src/App.js</code> and save to reload. But I should see this text.
         </p>
         <a
           className="App-link"
